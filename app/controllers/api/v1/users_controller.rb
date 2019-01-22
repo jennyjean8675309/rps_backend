3b6ddd722798521107b121549b4f319f76277845
jennyjean <jennyjean8675309@yahoo.com>
2019-01-22 16:21:00 -0500

    Added users #show action to usersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -23,6 +23,11 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
   def update
     @user = User.find(params[:id])
     render json: @user
