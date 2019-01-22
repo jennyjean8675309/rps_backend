73acb635d477836edcf253a76f57d8dc0912bc3a
jennyjean <jennyjean8675309@yahoo.com>
2019-01-22 10:43:36 -0500

    Added player stat attributes to UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password, :high_score
+  attributes :id, :username, :password, :high_score, :wins, :losses, :latest_stat, :consecutive_wins, :highest_consecutive_wins
 end
